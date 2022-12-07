0f4e266c86d041ac8f32775f16e068aa25ae4e17
Q <q@Qs-MacBook-Pro.local>
2022-12-08 08:02:39 +0900

    ephemeral container client app updates

--- ecc/main.go
+++ ecc/main.go
@@ -1,26 +1,27 @@
 package main
 
 import (
-    "context"
-    "fmt"
-    "flag"
-    "os"
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
 
-    "k8s.io/client-go/kubernetes"
-    "k8s.io/client-go/tools/clientcmd"
-    "k8s.io/apimachinery/pkg/api/errors"
-    corev1 "k8s.io/api/core/v1"
-    metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 func main() {
 	var (
-		nsTarget string
-		podTarget string
+		nsTarget        string
+		podTarget       string
 		containerTarget string
-		ecImage string
-		ecName string
-		kubeconfig string
+		ecImage         string
+		ecName          string
+		kubeconfig      string
 	)
 
 	flag.StringVar(&nsTarget, "ns", "default", "namespace to target")
@@ -37,86 +38,124 @@ func main() {
 		return
 	}
 
-    if containerTarget == "" {
-        fmt.Println("no target container info... exiting")
-        return
-    }
+	if containerTarget == "" {
+		fmt.Println("no target container info... exiting")
+		return
+	}
 
-    api, err := apiClientFromConfig(kubeconfig)
-    if err != nil {
-    panic(fmt.Errorf("could not create client: %w", err))
-    }
-    ctx := context.Background()
+	api, err := apiClientFromConfig(kubeconfig)
+	if err != nil {
+		panic(fmt.Errorf("could not create client: %w", err))
+	}
+	ctx := context.Background()
 
-    // Get the Pod
-    pod, err := api.
-        CoreV1().
-        Pods(nsTarget).
-        Get(ctx, podTarget, metav1.GetOptions{})
-    
-    if errors.IsNotFound(err) {
-        fmt.Printf("Target pod not found (%s/%s)\n", nsTarget, podTarget)
-    } else if statusError, isStatus := err.(*errors.StatusError); isStatus {
-        fmt.Printf("Error getting target pod (%s/%s): %v\n",
-            nsTarget, podTarget, statusError.ErrStatus.Message)
-    } else if err != nil {
-        panic(err)
-    }
-    
-    fmt.Printf("Found target pod (%s/%s)\n", nsTarget, podTarget)
+	pod, err := api.
+		CoreV1().
+		Pods(nsTarget).
+		Get(ctx, podTarget, metav1.GetOptions{})
 
-    ecInfo := newEphemeralContainerInfo(containerTarget, ecName, ecImage, nil, true)
-    pod.Spec.EphemeralContainers = append(pod.Spec.EphemeralContainers, ecInfo)
-    
-    _, err = api.
-        CoreV1().
-        Pods(pod.Namespace).
-        UpdateEphemeralContainers(ctx, pod.Name, pod, metav1.UpdateOptions{})
+	if errors.IsNotFound(err) {
+		fmt.Printf("Target pod not found (%s/%s)\n", nsTarget, podTarget)
+		return
+	} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
+		fmt.Printf("Error getting target pod (%s/%s): status = %v\n",
+			nsTarget, podTarget, statusError.ErrStatus.Message)
+		return
+	} else if err != nil {
+		fmt.Printf("Other error getting target pod: %v\n", err)
+		return
+	}
 
-    if err != nil {
-        panic(err)
-    }
+	fmt.Printf("Found target pod (%s/%s) [with %d ephemeral containers]\n",
+		nsTarget, podTarget, len(pod.Spec.EphemeralContainers))
+
+	ecInfo := newEphemeralContainerInfo(containerTarget, ecName, ecImage, nil, true)
+	pod.Spec.EphemeralContainers = append(pod.Spec.EphemeralContainers, ecInfo)
+
+	_, err = api.
+		CoreV1().
+		Pods(pod.Namespace).
+		UpdateEphemeralContainers(ctx, pod.Name, pod, metav1.UpdateOptions{})
+
+	if err != nil {
+		fmt.Printf("Error adding the ephemeral container to target pod: %v\n", err)
+		return
+	}
+
+	updatedPod, err := api.
+		CoreV1().
+		Pods(nsTarget).
+		Get(ctx, podTarget, metav1.GetOptions{})
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Added ephemeral container (%s/%s) to %s/%s/%s:\n",
+		ecName, ecImage, nsTarget, podTarget, containerTarget)
+
+	ec := ephemeralContainerFromPod(updatedPod, containerTarget, ecName)
+	if ec != nil {
+		ecData, _ := json.MarshalIndent(ec, "", " ")
+		fmt.Printf("%s\n\n", string(ecData))
+	} else {
+		fmt.Println("ephemeral container not found")
+	}
+}
+
+func ephemeralContainerFromPod(
+	pod *corev1.Pod,
+	target string,
+	name string) *corev1.EphemeralContainer {
+	for _, ec := range pod.Spec.EphemeralContainers {
+		if ec.TargetContainerName == target &&
+			ec.Name == name {
+			return &ec
+		}
+	}
+
+	return nil
 }
 
 func newEphemeralContainerInfo(
-    target string,    // target container in the pod
-    name string,      // name to use for the ephemeral container (must be unique)
-    image string,     // image to use for the ephemeral container
-    command []string, // custom ENTRYPOINT to use for the ephemeral container (yes, it's not CMD :-))
-    isPrivileged bool,// true if it should be a privileged container
-    ) corev1.EphemeralContainer {
-    isTrue := true
-    out := corev1.EphemeralContainer{
-        TargetContainerName: target,
-        EphemeralContainerCommon: corev1.EphemeralContainerCommon{
-            TTY:   true,
-            Stdin: true,            
-            Name:  name,
-            Image: image,
-            Command: command,
-        },
-    }
+	target string, // target container in the pod
+	name string, // name to use for the ephemeral container (must be unique)
+	image string, // image to use for the ephemeral container
+	command []string, // custom ENTRYPOINT to use for the ephemeral container (yes, it's not CMD :-))
+	isPrivileged bool, // true if it should be a privileged container
+) corev1.EphemeralContainer {
+	isTrue := true
+	out := corev1.EphemeralContainer{
+		TargetContainerName: target,
+		EphemeralContainerCommon: corev1.EphemeralContainerCommon{
+			TTY:     true,
+			Stdin:   true,
+			Name:    name,
+			Image:   image,
+			Command: command,
+		},
+	}
 
-    if isPrivileged {
-        out.EphemeralContainerCommon.SecurityContext = &corev1.SecurityContext{
-            Privileged: &isTrue,
-        }
-    }
+	if isPrivileged {
+		out.EphemeralContainerCommon.SecurityContext = &corev1.SecurityContext{
+			Privileged: &isTrue,
+		}
+	}
 
-    return out
+	return out
 }
 
 func apiClientFromConfig(kubeconfig string) (*kubernetes.Clientset, error) {
-    kubeconfig = os.ExpandEnv(kubeconfig)
+	kubeconfig = os.ExpandEnv(kubeconfig)
 
-    config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
-    if err != nil {
-        return nil, err
-    }
-    clientset, err := kubernetes.NewForConfig(config)
-    if err != nil {
-        return nil, err
-    }
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		return nil, err
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
 
-    return clientset, nil
+	return clientset, nil
 }
